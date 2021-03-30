6cd48651aaf4de18e609b12b98bda74fed656691
Igor Silva <igorhms@hotmail.com>
2021-03-30 18:45:53 -0300

    js atualizado

---
+++ js/functions.js
@@ -0,0 +1,20 @@
+function loadCustomer(){
+    xhtpp =XMLHttpRequest();
+    let file = ".../json/clientes.json"
+
+    xhtpp.onreadystatechance() = function () {
+        if((xhtpp.readyState == 4) && (xhtpp.status == 200)){
+            printCustomers(xhtpp.responseText);
+
+        }
+    }
+
+    xhtpp.open("GET", file, true);
+    xhtpp.send();
+}
+
+function printCustomers(cliente){
+    clientes = JSON.parse();
+    console.log(cliente.nome);
+    console.log(cliente.idade);
+}
